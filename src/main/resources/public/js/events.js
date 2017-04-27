04d4a7f1097cb6c06f2e42be82a550e70928463d
Mátyás Krista <nincs.matyas@gmail.com>
2017-04-27 14:48:23 +0200

    Done with cart counters, session in progress

--- src/main/resources/public/js/events.js
+++ src/main/resources/public/js/events.js
@@ -29,23 +29,38 @@ function deleteFromCart(id) {
 }
 
 function plusAmount(id) {
-    $("#amount-count").val(parseInt($("#amount-count").val()) + 1);
+    $("#amount-count"+id).val(parseInt($("#amount-count"+id).val()) + 1);
     addToCart(id);
-    $("#total-price").load();
+    setTotalPrice();
 }
 
 function minusAmount(id) {
-    $("#amount-count").val(parseInt($("#amount-count").val()) - 1);
-    if (parseInt($("#amount-count").val()) <= 0) {
+    $("#amount-count"+id).val(parseInt($("#amount-count"+id).val()) - 1);
+    if (parseInt($("#amount-count"+id).val()) <= 0) {
         deleteItem(id);
     }
     removeFromCart(id);
-    $("#total-price").load();
+    setTotalPrice();
 }
 
 function deleteItem(id) {
     $("#shopping-cart-"+id.toString()).empty().remove();
     $("#shopping-cart-row-"+id.toString()).empty().remove();
+    setTotalPrice();
     deleteFromCart(id);
-    $("#total-price").load();
+}
+
+function setTotalPrice () {
+    var total = 0;
+    $('#cart-items tr').each(function () {
+        //the value of sum needs to be reset for each row, so it has to be set inside the row loop
+        //find the combat elements in the current row and sum it
+        var price = parseFloat($(this).find('#price').text());
+        var amount = parseFloat($(this).find('.counter').val());
+        if (!isNaN(price) && !isNaN(amount)) {
+            total += parseFloat(price) * parseFloat(amount);
+        }
+    });
+    //set the value of currents rows sum to the total-combat element in the current row
+    $('#total-price-price').text(total + ' USD');
 }
