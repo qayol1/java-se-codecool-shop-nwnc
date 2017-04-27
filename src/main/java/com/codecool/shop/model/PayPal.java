11339b83de59a21f36e8c9819828b6cf1641e3da
qayol1 <qayol@freemail.hu>
2017-04-27 15:56:48 +0200

    pictures added

---
+++ src/main/java/com/codecool/shop/model/PayPal.java
@@ -0,0 +1,26 @@
+package com.codecool.shop.model;
+
+/**
+ * Created by peter on 2017.04.27..
+ */
+public class PayPal {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
