c4a192ccff6924dde5d3422df75e682ea99ce17f
Mátyás Krista <nincs.matyas@gmail.com>
2017-04-27 09:29:11 +0200

    Shopping cart plus minus added

--- src/main/resources/public/js/events.js
+++ src/main/resources/public/js/events.js
@@ -8,3 +8,23 @@ function addToCart(id) {
         }
     });
 }
+
+function removeFromCart(id) {
+    $.ajax({
+        method: 'POST',
+        url: '/add-to-cart',
+        data: {'id': id},
+        success: function () {
+            $("#cart-count").text(parseInt($("#cart-count").html()) - 1);
+        }
+    });
+}
+
+function plusAmount(id) {
+    $("#amount-count").val(parseInt($("#amount-count").val()) + 1);
+    addToCart(id);
+}
+
+function minusAmount(id) {
+    
+}
