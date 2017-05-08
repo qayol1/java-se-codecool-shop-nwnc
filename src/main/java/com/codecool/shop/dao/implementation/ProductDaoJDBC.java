4cdaea22a32b3223e596bffd7ac2759504eb1e7c
Abel Varadi <abelofvarad@gmail.com>
2017-05-08 15:40:30 +0200

    Initdb sql implemented, productdaojdbc work in progress

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
@@ -4,6 +4,9 @@ import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+import com.codecool.shop.util.DbConnect;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -13,6 +16,12 @@ import java.util.List;
  * Created by abelvaradi on 2017.05.08..
  */
 public class ProductDaoJDBC implements ProductDao {
+    private static ProductDaoJDBC instance = null;
+
+    private DbConnect dbConnect = new DbConnect("src/main/resources/connection/properties/connectionProperties.txt");
+
+    private ProductDaoJDBC() {
+    }
 
     public static ProductDaoJDBC getInstance() {
         if (instance == null) {
@@ -23,11 +32,64 @@ public class ProductDaoJDBC implements ProductDao {
 
     @Override
     public void add(Product product) {
-
+        try {
+            PreparedStatement stmt;
+            stmt = dbConnect.getConnection().prepareStatement(;
+                    ("INSERT INTO product" +
+                            "(name," +
+                            "description," +
+                            "defaultprice," +
+                            "currencystring," +
+                            "productcategory," +
+                            "supplier) VALUES (?, ?, ?, ?, ?, ?)"));
+            stmt.setString(1, product.getName());
+            stmt.setString(2, product.getDescription());
+            stmt.setFloat(3, product.getDefaultPrice());
+            stmt.setString(4, String.valueOf(product.getDefaultCurrency()));
+            stmt.setInt(5, product.getProductCategory().getId());
+            stmt.setInt(6, product.getSupplier().getId());
+            stmt.executeUpdate();
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Product find(int id) {
+
+        String query = "SELECT * FROM product WHERE id ='" + id + "';";
+
+        try (Connection connection = dbConnect.getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+
+        ){
+            ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+            SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+
+
+            while (resultSet.next()){
+
+
+                Product product = new Product(
+                        resultSet.getString("name"),
+                        resultSet.getFloat("defaultprice"),
+                        resultSet.getString("currencystring"),
+                        resultSet.getString("description"),
+                        productCategoryDataStore.find(resultSet.getInt("productcategory")),
+                        supplierDataStore.find(resultSet.getInt("supplier")));
+                product.setId(resultSet.getInt(1));
+                return product;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
 
     }
 
