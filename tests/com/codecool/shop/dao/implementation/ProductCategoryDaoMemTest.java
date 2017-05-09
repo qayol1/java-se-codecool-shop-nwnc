3a76811e53a8aed12cc94d520ab88b428c5d8ee3
qayol1 <qayol@freemail.hu>
2017-05-09 10:21:25 +0200

    dao mem class unit test classes added

---
+++ tests/com/codecool/shop/dao/implementation/ProductCategoryDaoMemTest.java
@@ -0,0 +1,17 @@
+package com.codecool.shop.dao.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by peter on 2017.05.09..
+ */
+class ProductCategoryDaoMemTest {
+    @Test
+    public void testIsVorking() {
+        assertEquals(10,10);
+
+    }
+
+}
