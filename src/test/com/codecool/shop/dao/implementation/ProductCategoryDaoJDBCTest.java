db976aea6b712f30ff570adafa53902a85432c0e
Abel Varadi <abelofvarad@gmail.com>
2017-05-10 17:09:42 +0200

    Productcategory jdbc test cases added

--- src/test/com/codecool/shop/dao/implementation/ProductCategoryDaoJDBCTest.java
+++ src/test/com/codecool/shop/dao/implementation/ProductCategoryDaoJDBCTest.java
@@ -1,41 +1,77 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.util.DbConnect;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Created by matyi on 2017.05.10..
- */
 class ProductCategoryDaoJDBCTest {
+
+    private String filePath = "src/main/resources/connection/properties/connectionPropertiesForTest.txt";
+
     @BeforeEach
     void setUp() {
+        DbConnect dbConnect = new DbConnect(filePath);
+        String query = "TRUNCATE TABLE productcategory, supplier, product";
+        try {
+            Statement statement = dbConnect.getConnection().createStatement();
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @AfterEach
     void tearDown() {
+
     }
 
     @Test
-    void getInstance() {
+    public void testIsProductCategoryDaoMemIsSingletone () {
+        ProductCategoryDao productCategoryDataStore1 = ProductCategoryDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore2 = ProductCategoryDaoMem.getInstance();
+        assertEquals(productCategoryDataStore1.hashCode(), productCategoryDataStore2.hashCode());
     }
 
     @Test
-    void add() {
+    public void testRemoveById() {
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoJDBC.getInstance();
+        ((ProductCategoryDaoJDBC)productCategoryDataStore).setConnection(filePath);
+        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer.");
+        productCategoryDataStore.add(tablet);
+        productCategoryDataStore.remove(1);
+        assertEquals(null,productCategoryDataStore.find(1));
     }
 
     @Test
-    void find() {
+    public void testRemoveByNonExistingId() {
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoJDBC.getInstance();
+        ((ProductCategoryDaoJDBC)productCategoryDataStore).setConnection(filePath);
+        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer.");
+        productCategoryDataStore.add(tablet);
+        assertFalse(productCategoryDataStore.remove(0));
     }
 
     @Test
-    void remove() {
+    public void testGetAllProductCategory() {
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoJDBC.getInstance();
+        ((ProductCategoryDaoJDBC)productCategoryDataStore).setConnection(filePath);
+        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet.");
+        productCategoryDataStore.add(tablet);
+        ProductCategory laptop = new ProductCategory("Laptop", "Hardware", "Is a small, portable personal computer.");
+        productCategoryDataStore.add(laptop);
+        ProductCategory videoCard = new ProductCategory("Video Card", "Hardware", "A video card.");
+        productCategoryDataStore.add(videoCard);
+        assertEquals(3, productCategoryDataStore.getAll().size());
     }
 
-    @Test
-    void getAll() {
-    }
 
 }
