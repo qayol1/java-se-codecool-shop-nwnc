4d9f35a3050fdb791e6bc85b432ed69ac399b84c
Mátyás Krista <nincs.matyas@gmail.com>
2017-05-10 13:42:13 +0200

    Database testing before method added

--- src/test/com/codecool/shop/dao/implementation/ProductDaoJDBCTest.java
+++ src/test/com/codecool/shop/dao/implementation/ProductDaoJDBCTest.java
@@ -1,8 +1,12 @@
 package com.codecool.shop.dao.implementation;
 
+import com.codecool.shop.util.DbConnect;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,8 +14,18 @@ import static org.junit.jupiter.api.Assertions.*;
  * Created by matyi on 2017.05.10..
  */
 class ProductDaoJDBCTest {
+
+    private DbConnect dbConnect = new DbConnect("src/main/resources/connection/properties/connectionPropertiesForTest.txt");
+
     @BeforeEach
     void setUp() {
+        String query = "TRUNCATE TABLE productcategory, supplier, product";
+        try {
+            Statement statement =dbConnect.getConnection().createStatement();
+            statement.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterEach
