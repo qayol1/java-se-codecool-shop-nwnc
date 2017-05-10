5aec02bfc266f64733fc77fa594fa4517f92a4c1
qayol1 <qayol@freemail.hu>
2017-05-10 09:53:22 +0200

    supplier dao mem unit tests added

--- src/test/com/codecool/shop/dao/implementation/SupplierDaoMemTest.java
+++ src/test/com/codecool/shop/dao/implementation/SupplierDaoMemTest.java
@@ -1,5 +1,9 @@
 package com.codecool.shop.dao.implementation;
 
+
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.Supplier;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,9 +13,68 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class SupplierDaoMemTest {
     @Test
-    public void testIsVorking() {
-        assertEquals(10,10);
+    public void testSameCategoryNameCannotAdd() {
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        Supplier amazon1 = new Supplier("Amazon", "Digital content and services");
+        supplierDataStore.add(amazon1);
+        Supplier amazon2 = new Supplier("Amazon", "Digital content and services");
+        supplierDataStore.add(amazon2);
+        int size = supplierDataStore.getAll().size();
+        assertEquals(1, size);
+    }
 
+    @Test
+    public void testIsProductCategoryDaoMemIsSingletone () {
+        SupplierDao supplierDataStore1 = SupplierDaoMem.getInstance();
+        SupplierDao supplierDataStore2 = SupplierDaoMem.getInstance();
+        assertEquals(supplierDataStore1.hashCode(),supplierDataStore2.hashCode());
+    }
+
+    @Test
+    public void testFindByIdIfNonExistingId() {
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        assertEquals(null,supplierDataStore.find(-4));
+    }
+
+    @Test
+    public void testFindByIfIdZero() {
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        assertEquals(null,productCategoryDataStore.find(0));
+    }
+
+    @Test
+    public void testRemoveById() {
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        Supplier amazon = new Supplier("Amazon", "Digital content and services");
+        supplierDataStore.add(amazon);
+        supplierDataStore.remove(1);
+        assertEquals(null,supplierDataStore.find(1));
+    }
+
+    @Test
+    public void testRemoveByNonExistingId() {
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        Supplier amazon = new Supplier("Amazon", "Digital content and services");
+        supplierDataStore.add(amazon);
+        assertFalse(supplierDataStore.remove(0));
+    }
+
+    @Test
+    public void testGetAllProductCategory() {
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        Supplier amazon = new Supplier("Amazon", "Digital content and services");
+        supplierDataStore.add(amazon);
+        Supplier lenovo = new Supplier("Lenovo", "Computers");
+        supplierDataStore.add(lenovo);
+        Supplier samsung = new Supplier("Samsung", "High-tech electronics manufacturing and digital media.");
+        supplierDataStore.add(samsung);
+        Supplier gigabyte = new Supplier("Gigabyte", "International manufacturer and distributor of computer hardware products.");
+        supplierDataStore.add(gigabyte);
+        Supplier msi = new Supplier("MSI", "International manufacturer and distributor of computer hardware products.");
+        supplierDataStore.add(msi);
+        Supplier asus = new Supplier("Asus", "International manufacturer and distributor of computer hardware products.");
+        supplierDataStore.add(asus);
+        assertEquals(6,supplierDataStore.getAll().size());
     }
 
 }
